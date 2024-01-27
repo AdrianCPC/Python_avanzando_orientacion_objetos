443d18d773eda7e821218d9b4263df258d1c5195
AdrianPC <adriancamilopc@gmail.com>
2024-01-26 22:25:59 -0500

    creating attributes likes

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 
 class Pelicula:
     def __init__(self, nombre, anio, duracion):
-        self.nombre = nombre
+        self.nombre = nombre.title()
         self.anio = anio
         self.duracion = duracion
+        self.me_gusta = 0
+
+    def gusta(self):
+        self.me_gusta += 1
 
 
 class Serie:
     def __init__(self, nombre, anio, temporadas):
-        self.nombre = nombre
+        self.nombre = nombre.title()
         self.anio = anio
         self.temporadas = temporadas
+        self.me_gusta = 0
+
+    def gusta(self):
+        self.me_gusta += 1
 
 
 #creando los objetos
@@ -18,5 +26,6 @@ class Serie:
 spr = Pelicula('Salvando al soldado ryan', 1998, 170)
 hl = Serie('Heartland', 2007, 16)
 
-print(f'El nombre de la pelicula es: {spr.nombre} - el año: {spr.anio} - la duración: {spr.duracion} minutos')
-print(f'El nombre de la serie es: {hl.nombre} - el año: {hl.anio} - el numero de temporadas: {hl.temporadas}')
+print(f'El nombre de la pelicula es: {spr.nombre} - el año: {spr.anio} - la duración: {spr.duracion} minutos. Cantidad Likes: {spr.me_gusta}')
+print(f'El nombre de la serie es: {hl.nombre} - el año: {hl.anio} - el numero de temporadas: {hl.temporadas}. Cantidad Likes: {hl.me_gusta}')
+
