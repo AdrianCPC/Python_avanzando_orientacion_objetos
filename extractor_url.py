8a2bd2b1e0043f37f5edc14c0a69e23c230318f8
AdrianPC <adriancamilopc@gmail.com>
2024-02-05 20:47:19 -0500

    using method find for url

--- extractor_url.py
+++ extractor_url.py
@@ -3,3 +3,12 @@ base_url = url[0:23]
 parametros_url = url[24:41]
 
 print(base_url, parametros_url)
+
+url2 = 'https://www.bytebank.com/cambio?monedaOrigen=usd&monedaDestino=cop&cantidad=100&operacion=venta'
+url2.find('?')
+
+indice_interrogacion = url2.find('?')
+base_url2 =url2[:indice_interrogacion]
+parametros_url2 = url2[indice_interrogacion + 1:]
+print(base_url2)
+print(parametros_url2)
