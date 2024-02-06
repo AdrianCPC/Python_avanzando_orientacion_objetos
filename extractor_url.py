4ac393a8d5a3f2d2feef2fdc7bda553c87bf00be
AdrianPC <adriancamilopc@gmail.com>
2024-02-06 17:52:19 -0500

    using cycles

--- extractor_url.py
+++ extractor_url.py
@@ -13,9 +13,25 @@ parametros_url2 = url2[indice_interrogacion + 1:]
 print(base_url2)
 print(parametros_url2)
 
-parametro_busqueda = 'operacion'
+parametro_busqueda = 'cantidad'
 indice_parametro = parametros_url2.find(parametro_busqueda)
 indice_valor = indice_parametro + len(parametro_busqueda) + 1
 
 valor = parametros_url2[indice_valor:]
+print(valor)
+
+
+
+
+
+url2 = 'https://www.bytebank.com/cambio?monedaOrigen=usd&monedaDestino=cop&cantidad=100&operacion=venta'
+parametro_busqueda = 'operacion'
+indice_parametro = parametros_url2.find(parametro_busqueda)
+indice_valor = indice_parametro + len(parametro_busqueda) + 1
+indice_and = parametros_url2.find('&', indice_valor)
+
+if indice_and == -1:
+    valor = parametros_url2[indice_valor:]
+else:
+    valor = parametros_url2[indice_valor:indice_and]
 print(valor)
