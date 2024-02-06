797229d27061e9a0ee61fbc88ddc09c00744144d
AdrianPC <adriancamilopc@gmail.com>
2024-02-05 21:00:45 -0500

    using method len

--- extractor_url.py
+++ extractor_url.py
@@ -12,3 +12,10 @@ base_url2 =url2[:indice_interrogacion]
 parametros_url2 = url2[indice_interrogacion + 1:]
 print(base_url2)
 print(parametros_url2)
+
+parametro_busqueda = 'operacion'
+indice_parametro = parametros_url2.find(parametro_busqueda)
+indice_valor = indice_parametro + len(parametro_busqueda) + 1
+
+valor = parametros_url2[indice_valor:]
+print(valor)
