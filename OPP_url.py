d80d8f35a92946e1b182a54c279e3f7943f41733
AdrianPC <adriancamilopc@gmail.com>
2024-02-13 14:50:29 -0500

    fix code for paramets

--- OPP_url.py
+++ OPP_url.py
@@ -2,29 +2,43 @@
 class ExtractorURL:
     #constructor
     def __init__(self, url):
-        self.url = url.limpia_url()
+        self.url = self.limpia_url(url)
         self.valida_url()
 
+        #Limpiar URL
     def limpia_url(self,url):
         return url.strip()
     
-    del valida_url(self):
-    if url == '':
-        raise ValueError('La URL esta vacia')
+    #Validar URL
+    def valida_url(self):
+        if self.url == '':
+            raise ValueError('La URL esta vacia')
     
+    #Obtencion URL base
     def get_base(self):
         indice_interrogacion = self.url.find('?')
         base_url = self.url[:indice_interrogacion]
         return base_url
-
+    
+    #Obtencion de parametros
     def get_parametros(self):
         indice_interrogacion = self.url.find('?')
         parametros_url = self.url[:indice_interrogacion]
         return parametros_url
 
-    def get_valor_parametros(self, parametro_busqueda):
-        pass
-    
+    def get_valor_parametro(self, parametro_busqueda):
+        parametro_busqueda = parametro_busqueda
+        indice_parametro = self.get_parametros().find(parametro_busqueda)
+        indice_valor = indice_parametro + len(parametro_busqueda) + 1
+        indice_and = self.get_parametros().find('&', indice_valor)
+        if indice_and == -1:
+            valor = self.get_parametros()[indice_valor]
+        else:
+            valor = self.get_parametros()[indice_valor:indice_and]
+
+        return valor
+
+     
 
 
 
